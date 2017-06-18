6be324ffda3898f193192d1cbb5be63562206b78
Clark Kampfe <clark.kampfe@gmail.com>
2017-06-18 13:20:56 -0500

    0.2.0

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Prolly.Mixfile do
 
   def project do
     [app: :prolly,
-     version: "0.1.0",
+     version: "0.2.0",
      elixir: "~> 1.4",
      package: package(),
      description: "Probabilistic data structures for Elixir",
